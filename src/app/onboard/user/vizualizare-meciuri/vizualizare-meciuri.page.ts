aece9e79ab3dad602259104ec9a2a5ecf257903a
Dragoi Anca-Florentina <67691293+DragoiAnca0404@users.noreply.github.com>
2024-09-13 15:28:39 +0300

    modify design

--- src/app/onboard/user/vizualizare-meciuri/vizualizare-meciuri.page.ts
+++ src/app/onboard/user/vizualizare-meciuri/vizualizare-meciuri.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MeciuriService } from '../../services/meciuri.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RegisterServiceService } from 'src/app/signup-signin/services/register-service.service';
 
 
 @Component({
@@ -10,17 +11,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class VizualizareMeciuriPage implements OnInit {
   meciuri: any[] = [];
-  scoruri: any[] = []; 
+  scoruri: any[] = [];
   denumireSport!: string | null;
+  isAdmin: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private meciuriService: MeciuriService,
-    private router: Router  
-
+    private router: Router,
+    private authService: RegisterServiceService
   ) { }
 
   ngOnInit(): void {
+    const role = this.authService.getRole();
+    console.log('User role:', role);
+    this.isAdmin = role === 'Admin';
+    console.log('isAdmin:', this.isAdmin);
+
     this.denumireSport = this.route.snapshot.paramMap.get('denumireSport');
 
     if (this.denumireSport) {
